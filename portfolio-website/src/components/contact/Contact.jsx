4e385332636e692b95ba72840502e522f3f85a2c
JalelBarhoumi <jalel.barhoumi@esprit.tn>
2022-06-11 00:00:54 +0100

    second commit

--- portfolio-website/src/components/contact/Contact.jsx
+++ portfolio-website/src/components/contact/Contact.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './Contact.css'
 import {MdOutlineEmail}from 'react-icons/md' 
 import{RiMessengerLine}from 'react-icons/ri'
 import{BsWhatsapp}from 'react-icons/bs'
+import emailjs from'emailjs-com'
 
 const Contact = () => {
+  const form=useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_slwj4ed', 'template_ronah1r', form.current, 'C6zhRytjHVLEWv9MO')
+    e.target.reset()  
+    .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -12,26 +25,31 @@ const Contact = () => {
       <div className="container    contact__container">
         <div className="contact__options">
         <article className="contact__option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>jalel.barhoumi@esprit.tn</h5>
             <a href='mailto:jalel.barhoumi@esprit.tn'target="blanck">Send a message</a>
           </article> 
         <article className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messanger</h4>
             <h5>Jalel Barhoumi</h5>
             <a href='https://m.me/profile.php?id=100005304675012'target="blanck">Send a message</a>
           </article>
           
           <article className="contact__option">
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+21623234701</h5>
             <a href='https://api.whatsapp.com/send?phone=+21623234701'target="blanck">Send a message</a>
           </article>
         </div>
-        <form action=''></form>
+        <form ref={form}onSubmit={sendEmail}>
+          <input type="text"name="name"placeholder='Your Full Name'required />
+          <input type="email"name="email"placeholder='Your Email'requiired/>
+          <textarea name="message"rows="7"placeholder='Your Message'required></textarea>
+          <button type="submit"className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
--- portfolio-website/src/components/footer/Footer.jsx
+++ portfolio-website/src/components/footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Footer.css'
-
+import {BsLinkedin} from 'react-icons/bs'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {MdFacebook} from 'react-icons/md'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#"className='footer__logo'>Jalel Barhoumi</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className='footer__socials' >
+        <a href="https://linkedin.com"target="blank"><BsLinkedin/></a>
+        <a href="https://instagram.com"target="blank"><AiOutlineInstagram/></a>
+        <a href="https://facebook.com"target="blank"><MdFacebook/></a>
+    </div>
+    <div className="footer__copyright">
+      <small>&copy;JalelBarhoumi, All rights reserved.</small>
+    </div>
+    </footer>
   )
 }
 
